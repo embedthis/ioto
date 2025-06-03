6d327f24f48e4f89a1c74c03097cb04e09d109b9
dev-embedthis <mob-pub-18@emobrien.com>
2025-06-03 17:13:37 +1000

    DEV: validation API fixes

--- lib/iotoLib.c
+++ lib/iotoLib.c
@@ -1665,7 +1665,7 @@ static int parseRegisterResponse(Json *json)
         return R_ERR_CANT_COMPLETE;
     }
     if (rEmitLog("debug", "ioto")) {
-        rDebug("ioto", "Device register response: %s", jsonString(json));
+        rDebug("ioto", "Device register response: %s", jsonString(json, JSON_PRETTY));
     }
     /*
         Response will have 2 elements when registered but not claimed
@@ -1688,7 +1688,7 @@ static int parseRegisterResponse(Json *json)
     jsonBlend(ioto->config, 0, "provision", json, 0, 0, 0);
 
     if (rEmitLog("debug", "ioto")) {
-        rDebug("ioto", "Provisioning: %s", jsonString(json));
+        rDebug("ioto", "Provisioning: %s", jsonString(json, JSON_PRETTY));
     }
 
     if (!ioto->nosave) {
@@ -2158,7 +2158,7 @@ PUBLIC int ioLoadConfig(void)
         rAddDirectory("site", "@state/site");
     }
     if (rEmitLog("debug", "ioto")) {
-        rDebug("ioto", "%s", jsonString(json));
+        rDebug("ioto", "%s", jsonString(json, JSON_PRETTY));
     }
     return 0;
 }
@@ -2515,26 +2515,25 @@ static int parseShow(cchar *arg)
 
 #if SERVICES_DATABASE
 /*
-    Write a database item and check against the API signature
-    Does not finalize
+    Write a database item as part of a response. Does not finalize the response.
+    Not validated against the API signature as it could be only part of the response.
  */
 PUBLIC ssize webWriteItem(Web *web, const DbItem *item)
 {
     if (!item) {
         return 0;
     }
-    return webWriteValidatedJson(web, dbJson(item), 0);
+    return webWrite(web, dbString(item, JSON_STRICT), -1);
 }
 
 /*
-    Write a database grid of items as a response. The items are validated against the API signature
-    and the request is finalized.
+    Write a database grid of items as part of a response. Does not finalize the response.
  */
 PUBLIC ssize webWriteItems(Web *web, RList *items)
 {
-    DbItem *item;
-    ssize  index, rc, wrote;
-    bool   prior;
+    DbItem   *item;
+    ssize    index, rc, wrote;
+    bool     prior;
 
     if (!items) {
         return 0;
@@ -2560,6 +2559,74 @@ PUBLIC ssize webWriteItems(Web *web, RList *items)
 }
 
 /*
+    Write a database item. DOES finalize the response.
+ */
+PUBLIC ssize webWriteValidatedItem(Web *web, const DbItem *item)
+{
+    ssize rc;
+
+    if (!item) {
+        return 0;
+    }
+    if (web->host->signatures && web->signature >= 0) {
+        rc = webWriteValidatedJson(web, dbJson(item));
+    } else {
+        rc = webWriteItem(web, item);
+    }
+    webFinalize(web);
+    return rc;
+}
+
+/*
+    Write a database grid of items as a response. Finalizes the response.
+ */
+PUBLIC ssize webWriteValidatedItems(Web *web, RList *items)
+{
+    Json     *json;
+    JsonNode *signature;
+    DbItem   *item;
+    ssize    index, rc, wrote;
+    bool     prior;
+
+    if (!items) {
+        return 0;
+    }
+    signature = 0;
+    if (web->host->signatures != 0 && web->signature >= 0) {
+        if ((signature = jsonGetNode(web->host->signatures, web->signature, "response.of")) == 0) {
+            webWriteResponse(web, 0, "Invalid signature for response");
+            return R_ERR_BAD_STATE;
+        }
+    }
+    rc = 0;
+    prior = 0;
+    webWrite(web, "[", 1);
+    for (ITERATE_ITEMS(items, item, index)) {
+        if (!item) {
+            continue;
+        }
+        json = (Json*) dbJson(item);
+        if (signature && !webValidateJsonSignature(web, "response", json, 0, signature, 0)) {
+            return R_ERR_BAD_ARGS;
+        }
+        if (prior) {
+            rc += webWrite(web, ",", -1);
+        }
+        //  validatedJson may be set by webValidateJsonSignature if fields are dropped
+        json = web->validatedJson ? web->validatedJson : json;
+        if ((wrote = webWriteJson(web, json)) <= 0) {
+            continue;
+        }
+        rc += wrote;
+        prior = 1;
+    }
+    rc += webWrite(web, "]", 1);
+    webFinalize(web);
+    return rc;
+}
+
+#if DEPRECATED && MOB && 0
+/*
     Write a database item as a response. The item is validated against the API signature
     and the request is finalized.
  */
@@ -2567,7 +2634,7 @@ PUBLIC ssize webWriteItemResponse(Web *web, const DbItem *item)
 {
     ssize rc;
 
-    rc = webWriteItem(web, item);
+    rc = webWriteValidatedItem(web, item);
     webFinalize(web);
     return rc;
 }
@@ -2580,10 +2647,11 @@ PUBLIC ssize webWriteItemsResponse(Web *web, RList *items)
 {
     ssize rc;
 
-    rc = webWriteItems(web, items);
+    rc = webWriteValidatedItems(web, items);
     webFinalize(web);
     return rc;
 }
+#endif
 
 /*
     Default login action. This is designed for web page use and redirects as a response (i.e. not for SPAs).
@@ -4282,7 +4350,7 @@ static void parseProvisioningResponse(Json *json)
 {
     cchar *certificate, *key;
     char  *certMem, *keyMem, *path;
-    bool   priorAccount;
+    bool  priorAccount;
 
     key = certificate = path = 0;
 
@@ -4326,7 +4394,7 @@ static void parseProvisioningResponse(Json *json)
     jsonBlend(ioto->config, 0, "provision", json, 0, 0, 0);
 
     if (rEmitLog("debug", "provision")) {
-        rDebug("provision", "%s", jsonString(json));
+        rDebug("provision", "%s", jsonString(json, JSON_PRETTY));
     }
 
     if (!ioto->nosave) {
@@ -5449,7 +5517,8 @@ static void processDeviceCommand(DbItem *item)
 
     cmd = dbField(item, "command");
 
-    rInfo("ioto", "Device command \"%s\"\nData: %s", cmd, jsonString(dbJson(item)));
+    rInfo("ioto", "Device command \"%s\"\nData: %s", cmd, dbString(item, JSON_PRETTY));
+
     if (smatch(cmd, "reboot")) {
         rSetState(R_RESTART);
 #if SERVICES_PROVISION
@@ -5592,7 +5661,7 @@ PUBLIC bool ioUpdate(void)
 
             /*
                 Download the update with throttling
-            */
+             */
             if (download(image, path) == 0) {
                 //  Validate
                 if (!checkSum(path, checksum)) {
