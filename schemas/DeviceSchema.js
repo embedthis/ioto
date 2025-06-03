262d572aab379e9b6bb9badc61b2ba86825c4114
dev-embedthis <mob-pub-18@emobrien.com>
2025-06-03 17:05:54 +1000

    FIX: DeviceSchema blending parts

--- schemas/DeviceSchema.js
+++ schemas/DeviceSchema.js
@@ -2,13 +2,16 @@
     DeviceSchema -- Core schema for the device-* databases
  */
 
+import blend from '@/paks/js-blend'
 import CloudSchema from './parts/CloudSchema.json5'
 import OneTable from './parts/OneTable.json5'
 import MetricSchema from './parts/MetricSchema.json5'
 
-export default Object.assign({
+let Schema = blend({
     version: 'latest',
     description: 'Default Device Cloud Schema',
     process: { },
     models: { },
 }, CloudSchema, MetricSchema, OneTable)
+
+export default Schema
